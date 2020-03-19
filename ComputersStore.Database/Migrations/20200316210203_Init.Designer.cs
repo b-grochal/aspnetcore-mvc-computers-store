db6e0f6013edc07b8ee30f550d90588f1ba26a91
Bartosz <radu480@o2.pl>
2020-03-19 12:18:44 +0100

    Create first migration.

--- ComputersStore.Database/Migrations/20200316210203_Init.Designer.cs
+++ ComputersStore.Database/Migrations/20200316210203_Init.Designer.cs
@@ -5,14 +5,16 @@ using ComputersStore.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace ComputersStore.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200316210203_Init")]
+    partial class Init
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
