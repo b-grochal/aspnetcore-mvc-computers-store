15595a83971a55648d61f4b192af59f87bd5e455
Bartosz <radu480@o2.pl>
2020-03-19 18:58:42 +0100

    Create viewmodel and business service for products.

---
+++ ComputersStore.BusinessServices/Implementation/ProductBusinessService.cs
@@ -0,0 +1,23 @@
+﻿using ComputersStore.BusinessServices.Interfaces;
+using ComputersStore.Models.ViewModels;
+using ComputersStore.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ComputersStore.BusinessServices.Implementation
+{
+    public class ProductBusinessService : IProductBusinessService
+    {
+        private readonly IProductService productService;
+        public ProductBusinessService(IProductService productService)
+        {
+            this.productService = productService;
+        }
+
+        public IEnumerable<ProductViewModel> GetProductsCollection()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
