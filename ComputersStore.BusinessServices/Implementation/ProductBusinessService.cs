ef469d9383a32afe4ee921316b1cc688e0e37a02
Bartosz <radu480@o2.pl>
2020-03-22 18:24:35 +0100

    Implement methods for products' business service.

--- ComputersStore.BusinessServices/Implementation/ProductBusinessService.cs
+++ ComputersStore.BusinessServices/Implementation/ProductBusinessService.cs
@@ -2,6 +2,7 @@
 using ComputersStore.BusinessServices.Interfaces;
 using ComputersStore.Core.Data;
 using ComputersStore.Models.ViewModels;
+using ComputersStore.Models.ViewModels.Basic;
 using ComputersStore.Services.Interfaces;
 using System;
 using System.Collections.Generic;
@@ -19,6 +20,25 @@ namespace ComputersStore.BusinessServices.Implementation
             this.mapper = mapper;
         }
 
+        public void AddProduct(NewProductViewModel newProductViewModel)
+        {
+            var product = MapNewProductViewModelToConcreteProduct(newProductViewModel);
+            productService.AddProduct(product); 
+        }
+
+        public void DeleteProduct(ProductViewModel productViewModel)
+        {
+            var product = MapProductViewModelToConcreteProduct(productViewModel);
+            productService.DeleteProduct(product);
+        }
+
+        public ProductDetailsViewModel GetProduct(int productId)
+        {
+            var product = productService.GetProduct(productId);
+            var result = mapper.Map<ProductDetailsViewModel>(product);
+            return result;
+        }
+
         public IEnumerable<ProductViewModel> GetProductsCollection()
         {
             var products = productService.GetProductsCollection();
@@ -31,6 +51,102 @@ namespace ComputersStore.BusinessServices.Implementation
 
             var products = productService.GetProductsCollection(productCategory, pageNumber, pageSize);
             var result = mapper.Map<IEnumerable<ProductViewModel>>(products);
+            return result;
+        }
+
+        public void UpdateProduct(ProductDetailsViewModel productDetailsViewModel)
+        {
+            var product = MapProductDetailsViewModelToConcreteProduct(productDetailsViewModel);
+            productService.UpdateProduct(product);
+        }
+
+        private Product MapNewProductViewModelToConcreteProduct(NewProductViewModel newProduct)
+        {
+            Product result = null;
+            switch(newProduct.ProductCategory)
+            {
+                case ProductCategory.CPU:
+                    result = mapper.Map<CentralProcessingUnit>(newProduct);
+                    break;
+                case ProductCategory.GPU:
+                    result = mapper.Map<GraphicsProcessingUnit>(newProduct);
+                    break;
+                case ProductCategory.HDD:
+                    result = mapper.Map<HardDiskDrive>(newProduct);
+                    break;
+                case ProductCategory.Motherboard:
+                    result = mapper.Map<Motherboard>(newProduct);
+                    break;
+                case ProductCategory.PSU:
+                    result = mapper.Map<PowerSupplyUnit>(newProduct);
+                    break;
+                case ProductCategory.RAM:
+                    result = mapper.Map<RandomAccessMemory>(newProduct);
+                    break;
+                case ProductCategory.SSD:
+                    result = mapper.Map<SolidStateDrive>(newProduct);
+                    break;
+            }
+            return result;
+        }
+
+        private Product MapProductViewModelToConcreteProduct(ProductViewModel productViewModel)
+        {
+            Product result = null;
+            switch (productViewModel.ProductCategory)
+            {
+                case ProductCategory.CPU:
+                    result = mapper.Map<CentralProcessingUnit>(result);
+                    break;
+                case ProductCategory.GPU:
+                    result = mapper.Map<GraphicsProcessingUnit>(result);
+                    break;
+                case ProductCategory.HDD:
+                    result = mapper.Map<HardDiskDrive>(result);
+                    break;
+                case ProductCategory.Motherboard:
+                    result = mapper.Map<Motherboard>(result);
+                    break;
+                case ProductCategory.PSU:
+                    result = mapper.Map<PowerSupplyUnit>(result);
+                    break;
+                case ProductCategory.RAM:
+                    result = mapper.Map<RandomAccessMemory>(result);
+                    break;
+                case ProductCategory.SSD:
+                    result = mapper.Map<SolidStateDrive>(result);
+                    break;
+            }
+            return result;
+        }
+
+        private Product MapProductDetailsViewModelToConcreteProduct(ProductDetailsViewModel productDetailsViewModel)
+        {
+            Product result = null;
+            switch (productDetailsViewModel.ProductCategory)
+            {
+                case ProductCategory.CPU:
+                    result = mapper.Map<CentralProcessingUnit>(result);
+                    break;
+                case ProductCategory.GPU:
+                    result = mapper.Map<GraphicsProcessingUnit>(result);
+                    break;
+                case ProductCategory.HDD:
+                    result = mapper.Map<HardDiskDrive>(result);
+                    break;
+                case ProductCategory.Motherboard:
+                    result = mapper.Map<Motherboard>(result);
+                    break;
+                case ProductCategory.PSU:
+                    result = mapper.Map<PowerSupplyUnit>(result);
+                    break;
+                case ProductCategory.RAM:
+                    result = mapper.Map<RandomAccessMemory>(result);
+                    break;
+                case ProductCategory.SSD:
+                    result = mapper.Map<SolidStateDrive>(result);
+                    break;
+            }
             return result;
         }
     }
