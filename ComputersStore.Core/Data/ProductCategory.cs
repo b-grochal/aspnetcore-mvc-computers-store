aae1a27a295ee37c2c6525e0ee51f3aa02da4730
Bartosz <radu480@o2.pl>
2020-06-11 00:24:50 +0200

    Updated create forms for products.

--- ComputersStore.Core/Data/ProductCategory.cs
+++ ComputersStore.Core/Data/ProductCategory.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Text;
 
 namespace ComputersStore.Core.Data
 {
     public enum ProductCategory
     {
+        [Description("CPU")]
         CPU = 1,
         Motherboard,
         RAM,
