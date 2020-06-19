a07c42c86d4541475a0c5a4742e82551492290e5
Bartosz <radu480@o2.pl>
2020-06-19 22:34:57 +0200

    Implemented view component for newsletter sign up form.

---
+++ ComputersStore/Components/NewsletterSignUpForm/NewsletterSignUpFormViewComponent.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ComputersStore.WebUI.Components.NewsletterSignUpForm
+{
+    public class NewsletterSignUpFormViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View();
+        }
+    }
+}
