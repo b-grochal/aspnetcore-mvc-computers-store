960a79794ff3b1ebbd7c4c34f5d11fe0b5117642
Bartosz <radu480@o2.pl>
2020-06-27 22:23:06 +0200

    Created orders' service with some methods.

---
+++ ComputersStore.BusinessServices/Implementation/OrderBusinessService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ComputersStore.BusinessServices.Implementation
+{
+    class OrderBusinessService
+    {
+    }
+}
---
+++ ComputersStore.BusinessServices/Interfaces/IOrderBusinessService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ComputersStore.BusinessServices.Interfaces
+{
+    public interface IOrderBusinessService
+    {
+
+    }
+}
