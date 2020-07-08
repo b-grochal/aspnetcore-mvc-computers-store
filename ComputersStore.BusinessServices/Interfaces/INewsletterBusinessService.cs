871b8ac8e45c6f946e73f8f72cf6f1b2c9486dd6
Bartosz <radu480@o2.pl>
2020-07-08 19:07:57 +0200

    Prepared async actions and methods for newsletters.

--- ComputersStore.BusinessServices/Interfaces/INewsletterBusinessService.cs
+++ ComputersStore.BusinessServices/Interfaces/INewsletterBusinessService.cs
@@ -2,11 +2,15 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace ComputersStore.BusinessServices.Interfaces
 {
     public interface INewsletterBusinessService
     {
-        void CreateNewsletter(NewsletterSignUpFormViewModel newsletterSignUpFormViewModel);
+        Task CreateNewsletter(NewsletterSignUpFormViewModel newsletterSignUpFormViewModel);
+        Task DeleteNewsletter(int newsletterId);
+        Task<IEnumerable<NewsletterViewModel>> GetNewslletersCollection();
+        Task<NewsletterViewModel> GetNewsletter(int newsletterId);
     }
 }
---
+++ ComputersStore/wwwroot/js/newsletter.js
@@ -0,0 +1,12 @@
+﻿function deleteNewsletter(newsletterId) {
+    $.ajax({
+        type: "POST",
+        url: "Newsletters/Delete",
+        data: {
+            id: newsletterId
+        },
+        success: function () {
+            $('#modal-placeholder').find('.modal').modal('hide');
+        }
+    })
+}
