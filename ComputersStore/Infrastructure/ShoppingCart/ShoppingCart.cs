7811e913287a94d3b5dd6cdfe77f2ebaff79aaa9
Bartosz <radu480@o2.pl>
2020-08-02 23:49:02 +0200

    Implemented basic shopping cart.

---
+++ ComputersStore/Infrastructure/ShoppingCart/ShoppingCart.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ComputersStore.WebUI.Infrastructure.ShoppingCart
+{
+    public class ShoppingCart
+    {
+        private List<ShoppingCartItem> shoppingCartItemsCollection = new List<ShoppingCartItem>(); 
+
+        public void AddItem(int productId)
+        {
+            var shoppingCartItem = shoppingCartItemsCollection
+                .Where(x => x.ProductId == productId)
+                .FirstOrDefault();
+
+            if(shoppingCartItem == null)
+            {
+                shoppingCartItemsCollection.Add(new ShoppingCartItem
+                {
+                    ProductId = productId,
+                    Quantity = 1
+                });
+            }
+            else
+            {
+                shoppingCartItem.Quantity++;
+            }
+        }
+
+        public void RemoweItem(int productId)
+        {
+            shoppingCartItemsCollection.RemoveAll(x => x.ProductId == productId);
+        }
+
+        public void Clear()
+        {
+            shoppingCartItemsCollection.Clear();
+        }
+
+        public IEnumerable<ShoppingCartItem> GetShoppingCartItems()
+        {
+            return shoppingCartItemsCollection;
+        } 
+    }
+}
