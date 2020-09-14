84604800010bf1333e7fb03a1eb23e94611a51db
Bartosz <radu480@o2.pl>
2020-09-15 00:30:36 +0200

    Implemeted searching through products by name. Refactored list action for products.

---
+++ ComputersStore.Models/ViewModels/Product/SearchedProductsListViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ComputersStore.Models.ViewModels.Product
+{
+    public class SearchedProductsListViewModel
+    {
+        public IEnumerable<ProductViewModel> Products { get; set; }
+        public string SearchString { get; set; }
+    }
+}
