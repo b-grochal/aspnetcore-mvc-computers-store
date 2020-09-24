db3cf7e3371a1163413cc9954a3b78801cce7f33
Bartosz <radu480@o2.pl>
2020-09-24 14:09:39 +0200

    Updated migrations.

--- ComputersStore.Database/Migrations/20200924120606_UpdateEntities.Designer.cs
+++ ComputersStore.Database/Migrations/20200924120606_UpdateEntities.Designer.cs
@@ -4,14 +4,16 @@ using ComputersStore.Database.DatabaseContext;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace ComputersStore.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200924120606_UpdateEntities")]
+    partial class UpdateEntities
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
