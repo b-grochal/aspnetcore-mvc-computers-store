c3eb671fa4d0a1cc57a4da8d279c4964784a4f1f
Bartosz <radu480@o2.pl>
2020-10-03 21:19:30 +0200

    Moved products' partials and modals into separate directories.

---
+++ ComputersStore.Models/ViewModels/Product/NewProductCategoryFormViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ComputersStore.Models.ViewModels.Product
+{
+    public class NewProductCategoryFormViewModel
+    {
+        public int ProductCategoryId { get; set; }
+    }
+}
