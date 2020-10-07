65332dd37f6d5fc4396515096b04fd423097ceff
Bartosz <radu480@o2.pl>
2020-10-07 13:13:55 +0200

    Created empty views and view models for new email messages.

---
+++ ComputersStore.EmailTemplates/Views/Emails/OrderStatusChangedEmail/OrderStatusChangedEmailViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ComputersStore.EmailTemplates.Views.Emails.OrderStatusChangedEmail
+{
+    class OrderStatusChangedEmailViewModel
+    {
+    }
+}
