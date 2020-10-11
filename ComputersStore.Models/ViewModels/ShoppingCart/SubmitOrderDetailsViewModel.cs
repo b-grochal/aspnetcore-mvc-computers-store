839f25abd7dce05d3b85680714187998809bdc09
Bartosz <radu480@o2.pl>
2020-10-11 12:25:28 +0200

    Added data annotations for viewmodels' properties.

--- ComputersStore.Models/ViewModels/ShoppingCart/SubmitOrderDetailsViewModel.cs
+++ ComputersStore.Models/ViewModels/ShoppingCart/SubmitOrderDetailsViewModel.cs
@@ -1,15 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace ComputersStore.Models.ViewModels.ShoppingCart
 {
     public class SubmitOrderDetailsViewModel
     {
+        [Required]
+        [Display(Name = "Payment type")]
         public int PaymentTypeId { get; set; }
+        
+        [Required]
+        [Display(Name = "Address")]
         public string ShipAddress { get; set; }
+
+        [Required]
+        [Display(Name = "City")]
         public string ShipCity { get; set; }
+        
+        [Required]
+        [Display(Name = "Postal code")]
         public string ShipPostalCode { get; set; }
+        
+        [Required]
+        [Display(Name = "Country")]
         public string ShipCountry { get; set; }
     }
 }
