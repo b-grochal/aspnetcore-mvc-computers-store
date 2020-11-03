5a026986b4e26f8dadc45d8bd0e0857984d9de1c
Bartosz <radu480@o2.pl>
2020-11-03 14:44:20 +0100

    Created specific viewmodels for editing products.

---
+++ ComputersStore.Models/ViewModels/Product/Specific/GraphicsProcessingUnit/GraphicsProcessingUnitEditFormViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ComputersStore.Models.ViewModels.Product.Specific.GraphicsProcessingUnit
+{
+    public class GraphicsProcessingUnitEditFormViewModel : ProductEditFormViewModel
+    {
+        public string MemorySize { get; set; }
+        public string MemoryType { get; set; }
+        public string MemorySpeed { get; set; }
+        public string MemoryBus { get; set; }
+        public string Interface { get; set; }
+    }
+}
---
+++ ComputersStore.Models/ViewModels/Product/Specific/PowerSupplyUnit/PowerSupplyUnitEditFormViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ComputersStore.Models.ViewModels.Product.Specific.PowerSupplyUnit
+{
+    public class PowerSupplyUnitEditFormViewModel : ProductEditFormViewModel
+    {
+        public string Size { get; set; }
+        public string Wattage { get; set; }
+    }
+}
