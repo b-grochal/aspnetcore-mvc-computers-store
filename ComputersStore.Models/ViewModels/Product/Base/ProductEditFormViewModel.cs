cf24cc5c7adecc32536545fbc1c02da05ba5b019
Bartosz <radu480@o2.pl>
2020-11-03 14:44:20 +0100

    Created specific viewmodels for editing products.

---
+++ ComputersStore.Models/ViewModels/Product/Base/ProductEditFormViewModel.cs
@@ -0,0 +1,20 @@
+﻿using ComputersStore.Data.Entities;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ComputersStore.Models.ViewModels.Product
+{
+    public abstract class ProductEditFormViewModel
+    {
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string ProductCategoryName { get; set; }
+        public int ProductCategoryId { get; set; }
+        public bool IsImageUpdated { get; set; }
+        public IFormFile ImageFile { get; set; }
+    }
+}
