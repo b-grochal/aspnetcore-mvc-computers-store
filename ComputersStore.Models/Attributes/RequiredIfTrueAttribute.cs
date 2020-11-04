2380ad47d12d27b27fdf7ebefd239e79438d0c09
Bartosz <radu480@o2.pl>
2020-11-04 22:02:47 +0100

    Created custom validation attributes for products.

---
+++ ComputersStore.Models/Attributes/RequiredIfTrueAttribute.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace ComputersStore.Models.Attributes
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    public class RequiredIfTrueAttribute : RequiredAttribute
+    {
+        private readonly string propertyName;
+        public RequiredIfTrueAttribute(string propertyName)
+        {
+            this.propertyName = propertyName;
+        }
+
+        protected override ValidationResult IsValid(object value, ValidationContext context)
+        {
+            object instance = context.ObjectInstance;
+            Type type = instance.GetType();
+
+            bool.TryParse(type.GetProperty(propertyName).GetValue(instance)?.ToString(), out bool propertyValue);
+
+            if (propertyValue && string.IsNullOrWhiteSpace(value?.ToString()))
+            {
+                return new ValidationResult(ErrorMessage);
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
