9af0a4953e5062df194dce01954dca0956989e90
Bartosz <radu480@o2.pl>
2020-11-04 22:02:47 +0100

    Created custom validation attributes for products.

--- ComputersStore.Models/ViewModels/Product/Base/ProductCreateFormViewModel.cs
+++ ComputersStore.Models/ViewModels/Product/Base/ProductCreateFormViewModel.cs
@@ -2,12 +2,14 @@
 using Microsoft.AspNetCore.Http;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace ComputersStore.Models.ViewModels.Product
 {
     public abstract class ProductCreateFormViewModel
     {
+        [Required]
         public string Name { get; set; }
         public string Description { get; set; }
         public decimal Price { get; set; }
