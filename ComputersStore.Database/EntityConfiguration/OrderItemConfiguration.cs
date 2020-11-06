20bf625a07c92057d83c2a1cfbe7f93d1baf533d
Bartosz <radu480@o2.pl>
2020-11-06 16:40:43 +0100

    Created configurations for entities. Enabled inserting explicit id values for some of entities.

---
+++ ComputersStore.Database/EntityConfiguration/OrderItemConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using ComputersStore.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ComputersStore.Database.EntityConfiguration
+{
+    public class OrderItemConfiguration : IEntityTypeConfiguration<OrderItem>
+    {
+        public void Configure(EntityTypeBuilder<OrderItem> builder)
+        {
+            builder.HasKey(orderItem => new { orderItem.OrderId, orderItem.ProductId });
+        }
+    }
+}
