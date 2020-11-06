2dea5f134f020f7113ba22ac0c0893c3606ffe73
Bartosz <radu480@o2.pl>
2020-11-06 16:40:43 +0100

    Created configurations for entities. Enabled inserting explicit id values for some of entities.

---
+++ ComputersStore.Database/EntityConfiguration/OrderConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using ComputersStore.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ComputersStore.Database.EntityConfiguration
+{
+    public class OrderConfiguration : IEntityTypeConfiguration<Order>
+    {
+        public void Configure(EntityTypeBuilder<Order> builder)
+        {
+            builder.HasKey(o => o.OrderId);
+
+            builder.Property(o => o.TotalCost)
+                .HasColumnType("decimal(18,2)");
+        }
+    }
+}
---
+++ ComputersStore.Database/EntityConfiguration/OrderStatusConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using ComputersStore.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ComputersStore.Database.EntityConfiguration
+{
+    public class OrderStatusConfiguration : IEntityTypeConfiguration<OrderStatus>
+    {
+        public void Configure(EntityTypeBuilder<OrderStatus> builder)
+        {
+            builder.HasKey(p => p.OrderStatusId);
+        }
+    }
+}
