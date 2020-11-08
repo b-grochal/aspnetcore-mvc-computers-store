d55897d13261b0a921d8142c9bae22828cf098cf
Bartosz <radu480@o2.pl>
2020-11-08 12:50:33 +0100

    Created new directory for custom tag helpers.

--- ComputersStore/TagHelpers/PaginationTagHelper.cs
+++ ComputersStore/TagHelpers/PaginationTagHelper.cs
@@ -9,7 +9,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace ComputersStore.WebUI.Infrastructure
+namespace ComputersStore.WebUI.TagHelpers
 {
     [HtmlTargetElement("pagination-links", Attributes = "pagination-view-model")]
     public class PaginationTagHelper : TagHelper
