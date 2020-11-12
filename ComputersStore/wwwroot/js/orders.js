10f1689aa1c7804e032ae7b0908102e705722bb2
Bartosz <radu480@o2.pl>
2020-11-12 22:41:57 +0100

    Implemented resetting filtering forms.

---
+++ ComputersStore/wwwroot/js/orders.js
@@ -0,0 +1,7 @@
+﻿function resetOrdersFilteringForm(e) {
+    e.preventDefault();
+    document.getElementById("OrderId").value = null;
+    document.getElementById("OrderStatusId").value = null;
+    document.getElementById("PaymentTypeId").value = null;
+    document.getElementById("ApplicationUserEmail").value = "";
+}
