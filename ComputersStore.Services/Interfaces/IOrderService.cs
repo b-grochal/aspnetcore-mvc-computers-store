9417d8d962fb2c892899177318f37d5b929c99a8
Bartosz <radu480@o2.pl>
2020-11-16 22:05:04 +0100

    Fixed orders' table in admin panel.

--- ComputersStore.Services/Interfaces/IOrderService.cs
+++ ComputersStore.Services/Interfaces/IOrderService.cs
@@ -9,6 +9,7 @@ namespace ComputersStore.Services.Interfaces
     public interface IOrderService
     {
         Task<IEnumerable<Order>> GetOrdersCollection(int? orderId, int? orderStatusId, int? paymentTypeId, string userEmail, int pageNumber, int pageSize);
+        Task<int> GetOrdersCollectionCount(int? orderId, int? orderStatusId, int? paymentTypeId, string userEmail);
         Task<Order> GetOrder(int orderId);
         Task<int> CreateOrder(Order order);
         Task UpdateOrder(Order order);
