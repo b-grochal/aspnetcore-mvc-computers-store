8c48fd58fc0b9d7dbe387a790d88fbf4a938fde4
Bartosz <radu480@o2.pl>
2020-11-17 12:25:17 +0100

    Fixed recommended products' card.

--- ComputersStore.Database/Migrations/ApplicationDbContextModelSnapshot.cs
+++ ComputersStore.Database/Migrations/ApplicationDbContextModelSnapshot.cs
@@ -6,7 +6,7 @@ using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
-namespace ComputersStore.Data.Migrations
+namespace ComputersStore.Database.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
     partial class ApplicationDbContextModelSnapshot : ModelSnapshot
